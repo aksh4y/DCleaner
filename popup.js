dfdc76d6dce1b714035378c2b7389a9c1fd2673a
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2017-05-10 01:57:22 +0530

    Remove badge when 0 items

--- popup.js
+++ popup.js
@@ -8,12 +8,14 @@ window.onload = function () {
             chrome.downloads.search({
                 "exists" : false,
             } , function (downloadedItem) {
+                if(downloadedItem.length === 0)
+                    chrome.browserAction.setBadgeText({text: ""});
                 for (var i =0; i < downloadedItem.length; i++){
                     chrome.downloads.erase({
                         "id" : downloadedItem[i].id
                     }, function (res) {
                         chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-                        chrome.browserAction.setBadgeText({text: "0"});
+                        chrome.browserAction.setBadgeText({text: ""});
                     });
                 }
             });
